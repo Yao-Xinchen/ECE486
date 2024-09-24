a682cb04313a4e0ce41062cb8b1dcd06ead9e855
Yao-Xinchen <yao29@illinois.edu>
2024-09-23 20:53:21 -0500

    complete lab2

--- Lab2/lab2part1plot.m
+++ Lab2/lab2part1plot.m
@@ -20,3 +20,16 @@ plot(tout1, y1dot);
 
 legend("y1", "y1dot");
 xlabel("time, s");
+
+yss = y1(end);
+ymax = max(y1);
+mp = (ymax - yss) / yss * 100;
+t10_index = find(y1 > 0.1 * ymax, 1, "first");
+t90_index = find(y1 > 0.9 * ymax, 1, "first");
+tr = tout1(t90_index) - tout1(t10_index);
+x = length(y1);
+while y1(x) < 1.05 * yss && y1(x) > 0.95 * yss
+    x = x - 1;
+end
+ts = tout1(x);
+fprintf("no zero: M_p=%.2f%%, t_r=%.2fs, t_s=%.2fs\n", mp, tr, ts);
--- Lab2/lab2part3plot.m
+++ Lab2/lab2part3plot.m
@@ -1,3 +1,7 @@
+clear
+clc
+close all
+
 A = [-6 -25; 1 0];
 B = [25; 0];
 C = [1 0; 0 1];
@@ -20,6 +24,20 @@ for i = 1:length(as)
     tout2 = out.tout;
     y2 = out.y;
     plot(tout2, y2);
+
+    zero = -a * zeta;
+    yss = y2(end);
+    ymax = max(y2);
+    mp = (ymax - yss) / yss * 100;
+    t10_index = find(y2 > 0.1 * ymax, 1, "first");
+    t90_index = find(y2 > 0.9 * ymax, 1, "first");
+    tr = tout2(t90_index) - tout2(t10_index);
+    x = length(y2);
+    while y2(x) < 1.05 * yss && y2(x) > 0.95 * yss
+        x = x - 1;
+    end
+    ts = tout2(x);
+    fprintf("zero=%.1f: M_p=%.2f%%, t_r=%.2fs, t_s=%.2fs\n", zero, mp, tr, ts);
 end
 
 legend("y1", "y3a", "y3b", "y3c");
