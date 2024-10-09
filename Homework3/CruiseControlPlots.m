6cb3eecd9d5f3484dfd32c5eab53509a60bcf222
Yao-Xinchen <yao29@illinois.edu>
2024-10-08 20:18:43 -0500

    complete hw2p2

--- Homework3/CruiseControlPlots.m
+++ Homework3/CruiseControlPlots.m
@@ -1,4 +1,7 @@
 %% Cruise Control Problem
+clear;
+clc;
+close all;
 
 %% Vehicle Parameters
 m = 2085;       % mass, kg
@@ -10,20 +13,49 @@ Froll = 228;    % Rolling resistance, N
 %% A) Open-Loop Input
 % Assume Fgrav=0 and compute the constant input required to 
 % maintain vdes=29m/sec
-ubar=29;
+v0 = 0;
+Fgrav = 0;
+ubar = (b1 * 29 + (Froll + b0)) / k;
+fprintf("ubar = %f\n", ubar);
 
 %% B) Simulation with open-loop input and disturbance
 
 % Simulation Parameters
 v0 = 29;        % Initial condition for velocity, m/sec
 Tf = 50;        % Final simulation time, sec
+Fgrav = 350;
 
 % Simulate System
+out = sim('CruiseControlSim.slx');
 
 % Generate Plot
+figure
+plot(out.tout, out.v);
+ylim([20, 30]);
+title("Velocity vs Time with Open-Loop Input and Disturbance");
+ylabel("Velocity (m/s)");
+xlabel("Time (s)");
 
 %% C) Selection of PI gains
 
+kp = 30.7;
+ki = 4.69;
 
 %% D) Simulation with PI and disturbance
+Fgrav = 1400;
+v_des = 29;
 
+out = sim('CruiseControlSimClose.slx');
+figure
+subplot(2, 1, 1);
+plot(out.tout, out.v_cl);
+title("Velocity vs Time with PI Controller and Disturbance");
+ylim([28, 30]);
+ylabel("Velocity (m/s)");
+xlabel("Time (s)");
+
+subplot(2, 1, 2);
+plot(out.tout, out.u);
+title("Control Input vs Time with PI Controller and Disturbance");
+ylabel("Throttle input (deg)");
+xlabel("Time (s)");
